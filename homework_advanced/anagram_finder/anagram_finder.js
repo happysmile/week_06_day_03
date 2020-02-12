e87051671584c7d4ef548302baa6e92c1e180238
user <mancusofrancesca@hotmail.com>
2020-02-12 17:03:19 +0000

    started working on anagram finder - hopefully finishing later tonight

--- homework_advanced/anagram_finder/anagram_finder.js
+++ homework_advanced/anagram_finder/anagram_finder.js
@@ -1,9 +1,29 @@
 const AnagramFinder = function (word) {
+  this.word = word;
+}
 
+AnagramFinder.prototype.splitInLetters = function (string) {
+  return string.split('');
+}
+
+AnagramFinder.prototype.allLowerCase = function (string) {
+  return string.toLowerCase();
+}
+
+AnagramFinder.prototype.compareArrays = function (array1, array2) {
+  // return true if they have the same elements
+  // return false if they don't
 }
 
 AnagramFinder.prototype.findAnagrams = function (otherWords) {
-
+  let lowercaseword = this.allLowerCase(this.word);
+  let wordInLetters = lowercaseword.splitInLetters();
+  otherWordsInLetters = otherWords.map(word => {
+    return this.allLowerCase(word).splitInLetters();
+  });
+  const allAnagrams = otherWordsInLetters.filter( (otherWordInLetter) => {
+    return this.compareArrays(otherWordInLetter, wordInLetters);
+  });
 }
 
 module.exports = AnagramFinder;
